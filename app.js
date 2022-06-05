1edc0c77022066d640965f859bcb5983ed1c6a6f
kandalog <asuasu.tee@gmail.com>
2022-06-05 13:49:25 +0900

    全体コンパイルを実装 + ディレクトリ整理

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-// const add = (num1:number, num2:number) => num1 + num2
-// let func: (a: number, b: number) => number;
-// func = add;
-// console.log(add(8,10))
-// 引数を一つだけ取る関数がcallbackに入る
-// callbackの引数は第1引数と第2引数を合計した値が自動で入る
-// それを実行する (実行されて行われる処理は呼び出し時に決めることができる)
-function hoge(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-hoge(10, 20, function (result) {
-    console.log(result);
-});
+"use strict";
+const userName = 'Max';
+console.log(userName);
